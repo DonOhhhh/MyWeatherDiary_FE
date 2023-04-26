1efcce7dbb05fd003e7643fe1f267d05def0dea7
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-04-26 18:06:04 +0900

    small updates

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/proxy": {
-                target: "http://192.168.0.47:8080",
+                target: "http://localhost:8080",
                 changeOrigin: true,
-                rewrite: (path) => path.replace(/\/proxy/, "/api/v1"),
+                rewrite: (path) => path.replace(/^\/proxy/, "/api/v1"),
             },
         },
     },
@@ -22,7 +22,7 @@ export default defineConfig({
             "/proxy": {
                 target: "http://ip-10-10-4-10.ap-northeast-2.compute.internal:8080",
                 changeOrigin: false,
-                rewrite: (path) => path.replace(/\/proxy/, "/api/v1"),
+                rewrite: (path) => path.replace(/^\/proxy/, "/api/v1"),
             },
         },
     },
